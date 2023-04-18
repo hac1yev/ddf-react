78e75b9cbabef994160257a385b7af88faae7b5d
Haciyev ilkin <ilkinhaciyev955@gmail.com>
2023-04-18 11:25:49 +0400

    rehberlik

--- src/pages/Rehberlik.jsx
+++ src/pages/Rehberlik.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import '../assets/css/RehberlikModal.css';
-import { GlobalContext } from "./GlobalState";
-import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../assets/api/dataFetching";
