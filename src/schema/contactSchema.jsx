aa92d5c1e60ee67c8bb4ffdf65b5ff241cb7fe9d
hac1yev <99089581+hac1yev@users.noreply.github.com>
2023-10-31 12:21:37 +0400

    Update contactSchema.jsx

--- src/schema/contactSchema.jsx
+++ src/schema/contactSchema.jsx
@@ -7,7 +7,7 @@ const regexPhone = /[+]994(40|5[015]|60|7[07])\d{7}/;
 const language = window.localStorage.getItem('lang');
 
 export const contactSchema = yup.object().shape({
-    email: yup.string().email(language === 'az' ? 'Xaiş edirik düzgün nömrə daxil edin!' : 'Please enter a valid email!').required(language === 'az' ? "Email tələb olunur!" : "Email required!"),
+    email: yup.string().email(language === 'az' ? 'Xaiş edirik düzgün email daxil edin!' : 'Please enter a valid email!').required(language === 'az' ? "Email tələb olunur!" : "Email required!"),
     phone: yup.string().matches(regexPhone, language === 'az' ? 'Xaiş edirik düzgün nömrə daxil edin!' : 'Please enter a valid phone number').required(language === 'az' ? "Telefon nömrəsi tələb olunur!" : "Phone number required"),
     name: yup.string().required(language === 'az' ? "Ad və soyadınızı daxil edin!" : "Please enter your name and surname!"),
     address: yup.string().required(language === 'az' ? "Ünvanınızı daxil edin!" : "Please enter your address!"),
